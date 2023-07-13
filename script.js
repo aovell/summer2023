43d1a6248ffef3dc5b7204721fa460c93af3584d
aovell <135627614+aovell@users.noreply.github.com>
2023-07-13 14:00:56 -0400

    commit

--- script.js
+++ script.js
@@ -139,47 +139,79 @@ $.ajax({
         
 //
 
-// script.js book details
 $(document).ready(function() {
-  var urlParams = new URLSearchParams(window.location.search);
-  var bookId = urlParams.get('bookId');
-  if (bookId) {
-    getBookDetails(bookId);
-  } else {
-    $('#book-details').html('<p>No book ID found.</p>');
+  // Define variables
+  var apiUrl = 'https://www.googleapis.com/books/v1/volumes?q=';
+  var maxResults = 60;
+  var resultsPerPage = 10;
+  var currentPage = 1;
+  var totalResults = 0;
+  var totalPages = 0;
+
+  // Handle search button click
+  $('#search-button').click(function() {
+    var searchTerm = $('#search-input').val();
+    if (searchTerm.trim() !== '') {
+      // Construct API URL
+      var url = apiUrl + encodeURIComponent(searchTerm) + '&maxResults=' + maxResults;
+      
+      // Call API and process response
+      $.getJSON(url, function(response) {
+        if (response.items) {
+          totalResults = response.items.length;
+          totalPages = Math.ceil(totalResults / resultsPerPage);
+          currentPage = 1;
+          displayResults(currentPage);
+        } else {
+          $('#results-container').html('No results found.');
+        }
+      });
+    }
+  });
+
+  // Display search results for the specified page
+  function displayResults(page) {
+    var startIndex = (page - 1) * resultsPerPage;
+    var endIndex = startIndex + resultsPerPage;
+    var books = response.items.slice(startIndex, endIndex);
+    var html = '';
+    for (var i = 0; i < books.length; i++) {
+      var book = books[i].volumeInfo;
+      html += '<div class="book-item">';
+      if (book.imageLinks && book.imageLinks.smallThumbnail) {
+        html += '<img src="' + book.imageLinks.smallThumbnail + '" alt="' + book.title + '">';
+      }
+      html += '<div class="book-info">';
+      html += '<h2 class="book-title">' + book.title + '</h2>';
+      if (book.description) {
+        html += '<p class="book-description">' + book.description + '</p>';
+      }
+      html += '</div></div>';
+    }
+    $('#results-container').html(html);
+    displayPagination(page);
+  }
+
+  // Display pagination controls
+  function displayPagination(page) {
+    var html = '';
+    if (totalPages > 1) {
+      html += '<select id="page-select">';
+      for (var i = 1; i <= totalPages; i++) {
+        html += '<option value="' + i + '"';
+        if (i === page) {
+          html += ' selected';
+        }
+        html += '>' + i + '</option>';
+      }
+      html += '</select>';
+    }
+    $('#results-container').append(html);
+
+    // Handle page select change
+    $('#page-select').change(function() {
+      var selectedPage = parseInt($(this).val());
+      displayResults(selectedPage);
+    });
   }
 });
-
-function getBookDetails(bookId) {
-  var apiUrl = 'https://www.googleapis.com/books/v1/volumes/' + bookId;
-  
-  $.getJSON(apiUrl, function(data) {
-    displayBookDetails(data);
-  });
-}
-
-function displayBookDetails(data) {
-  var detailsContainer = $('#book-details');
-  detailsContainer.empty();
-  
-  var book = data.volumeInfo;
-  
-  var title = book.title;
-  var authors = book.authors ? book.authors.join(', ') : 'Unknown';
-  var publisher = book.publisher || 'Unknown';
-  var description = book.description || 'No description available.';
-  var coverImage = book.imageLinks ? book.imageLinks.thumbnail : 'no-image.jpg';
-  var price = book.saleInfo && book.saleInfo.listPrice ? book.saleInfo.listPrice.amount + ' ' + book.saleInfo.listPrice.currencyCode : 'Not for sale';
-  
-  var titleElement = $('<h2>').text(title);
-  var authorsElement = $('<p>').text('Author(s): ' + authors);
-  var publisherElement = $('<p>').text('Publisher: ' + publisher);
-  var descriptionElement = $('<p>').text(description);
-  var coverImageElement = $('<img>').attr('src', coverImage);
-  var priceElement = $('<p>').text('Price: ' + price);
-  
-  detailsContainer.append(titleElement, authorsElement, publisherElement, descriptionElement, coverImageElement, priceElement);
-}
-
-
-   
