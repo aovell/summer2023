0fefc411970c9dbec41872860e5d5d74cec02ade
aovell <135627614+aovell@users.noreply.github.com>
2023-07-24 15:11:59 -0400

    commit

--- script.js
+++ script.js
@@ -1,354 +1,223 @@
+const apiKey = "010673d20468141444290cf8f7d499ae";
+const apiUrl = "https://api.themoviedb.org/3";
 
-// book search
-$(document).ready(function() {
-  $('#search-form').submit(function(event) {
-    event.preventDefault();
-    var searchTerm = $('#search-term').val();
-    searchBooks(searchTerm);
-  });
-});
-
-function searchBooks(searchTerm) {
-  var apiUrl = 'https://www.googleapis.com/books/v1/volumes?q=' + encodeURIComponent(searchTerm);
-  
-  $.getJSON(apiUrl, function(data) {
-    displaySearchResults(data);
-  });
-}
-
-function displaySearchResults(data) {
-  var resultsContainer = $('#search-results');
-  resultsContainer.empty();
-  
-  if (data.totalItems === 0) {
-    resultsContainer.html('<p>No results found.</p>');
-    return;
-  }
-  
-  var books = data.items.slice(0, 50); // Get first 50 books
-  
-  $.each(books, function(index, book) {
-    var title = book.volumeInfo.title;
-    var coverImage = book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.smallThumbnail : 'no-image.jpg';
-    var bookId = book.id;
-    
-    var bookElement = $('<div class="book-item">');
-    var titleElement = $('<h2>').text(title);
-    var coverImageElement = $('<img>').attr('src', coverImage);
-    
-    var bookLink = $('<a>').attr('href', 'bookDetails.html?bookId=' + bookId).append(titleElement);
-    
-    bookElement.append(bookLink, coverImageElement);
-    resultsContainer.append(bookElement);
-  });
-}
-
-$(document).ready(function() {
-  var urlParams = new URLSearchParams(window.location.search);
-  var bookId = urlParams.get('bookId');
-  if (bookId) {
-    getBookDetails(bookId);
-  } else {
-    $('#book-details').html('<p>No book ID found.</p>');
-  }
-});
-
-function getBookDetails(bookId) {
-  var apiUrl = 'https://www.googleapis.com/books/v1/volumes/' + bookId;
-  
-  $.getJSON(apiUrl, function(data) {
-    displayBookDetails(data);
-  });
-}
-
-function displayBookDetails(data) {
-  var detailsContainer = $('#book-details');
-  detailsContainer.empty();
-  
-  var book = data.volumeInfo;
-  
-  var title = book.title;
-  var authors = book.authors ? book.authors.join(', ') : 'Unknown';
-  var publisher = book.publisher || 'Unknown';
-  var description = book.description || 'No description available.';
-  var coverImage = book.imageLinks ? book.imageLinks.thumbnail : 'no-image.jpg';
-  var price = book.saleInfo && book.saleInfo.listPrice ? book.saleInfo.listPrice.amount + ' ' + book.saleInfo.listPrice.currencyCode : 'Not for sale';
-  
-  var titleElement = $('<h2>').text(title);
-  var authorsElement = $('<p>').text('Author(s): ' + authors);
-  var publisherElement = $('<p>').text('Publisher: ' + publisher);
-  var descriptionElement = $('<p>').text(description);
-  var coverImageElement = $('<img>').attr('src', coverImage);
-  var priceElement = $('<p>').text('Price: ' + price);
-  
-  detailsContainer.append(titleElement, authorsElement, publisherElement, descriptionElement, coverImageElement, priceElement);
-}
-
-
-var view = 'grid'; // Default view is grid
-
-function handleResponse(response) {
-  var bookshelf = document.getElementById('bookshelf');
-
-  if (response && response.items) {
-    var books = response.items;
-    bookshelf.innerHTML = ''; // Clear previous content
-
-    if (view === 'grid') {
-      bookshelf.classList.add('grid');
-    } else if (view === 'list') {
-      bookshelf.classList.add('list');
+function searchByTitle() {
+    const searchInput = document.getElementById("searchInput").value.trim();
+    if (searchInput === "") {
+        alert("Please enter a title to search.");
+        return;
     }
 
-    for (var i = 0; i < books.length; i++) {
-      var book = books[i].volumeInfo;
-      var title = book.title;
-      var authors = book.authors ? book.authors.join(', ') : 'Unknown Author';
-      var thumbnail = book.imageLinks ? book.imageLinks.smallThumbnail : '';
-      var description = book.description ? book.description : 'No description available.';
-      var previewLink = book.previewLink;
-
-      var bookElement = document.createElement('div');
-      bookElement.classList.add('book');
-
-      var imageElement = document.createElement('img');
-      imageElement.src = thumbnail;
-
-      var titleElement = document.createElement('h3');
-      titleElement.innerHTML = title;
-
-      var authorElement = document.createElement('p');
-      authorElement.innerHTML = 'By ' + authors;
-
-      var descriptionElement = document.createElement('p');
-      descriptionElement.innerHTML = description;
-
-      var previewLinkElement = document.createElement('a');
-      previewLinkElement.href = previewLink;
-      previewLinkElement.innerHTML = 'Read more';
-
-      bookElement.appendChild(imageElement);
-      bookElement.appendChild(titleElement);
-      bookElement.appendChild(authorElement);
-      bookElement.appendChild(descriptionElement);
-      bookElement.appendChild(previewLinkElement);
-
-      bookshelf.appendChild(bookElement);
-    }
-  } else {
-    bookshelf.innerHTML = 'No books found.';
-  }
+    const url = `${apiUrl}/search/movie?api_key=${apiKey}&query=${searchInput}`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => displayResults(data.results))
+        .catch((error) => console.log(error));
 }
 
-// Replace YOUR_BOOKSHELF_ID with the ID of your public bookshelf
-var booksApiUrl = 'https://www.googleapis.com/books/v1/users/114034464592823534860/bookshelves/1001/volumes?callback=handleResponse';
-$.ajax({
-  url: booksApiUrl,
-  dataType: 'jsonp',
-  success: handleResponse
-});
-
-function switchView(newView) {
-  var bookshelf = document.getElementById('bookshelf');
-  bookshelf.classList.remove(view);
-
-  view = newView;
-
-  if (view === 'grid') {
-    bookshelf.classList.add('grid');
-  } else if (view === 'list') {
-    bookshelf.classList.add('list');
-  }
+function discoverPopularMovies() {
+    const url = `${apiUrl}/discover/movie?api_key=${apiKey}&sort_by=popularity.desc`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => displayResults(data.results))
+        .catch((error) => console.log(error));
 }
 
-//
+function displayResults(results) {
+    const resultsDiv = document.getElementById("results");
+    resultsDiv.innerHTML = "";
 
-$(document).ready(function() {
-  // Define variables
-  var apiUrl = 'https://www.googleapis.com/books/v1/volumes?q=';
-  var maxResults = 60;
-  var resultsPerPage = 10;
-  var currentPage = 1;
-  var totalResults = 0;
-  var totalPages = 0;
-
-  // Handle search button click
-  $('#search-button').click(function() {
-    var searchTerm = $('#search-input').val();
-    if (searchTerm.trim() !== '') {
-      // Construct API URL
-      var url = apiUrl + encodeURIComponent(searchTerm) + '&maxResults=' + maxResults;
-      
-      // Call API and process response
-      $.getJSON(url, function(response) {
-        if (response.items) {
-          totalResults = response.items.length;
-          totalPages = Math.ceil(totalResults / resultsPerPage);
-          currentPage = 1;
-          displayResults(currentPage);
-        } else {
-          $('#results-container').html('No results found.');
-        }
-      });
-    }
-  });
-
-  // Display search results for the specified page
-  function displayResults(page) {
-    var startIndex = (page - 1) * resultsPerPage;
-    var endIndex = startIndex + resultsPerPage;
-    var books = response.items.slice(startIndex, endIndex);
-    var html = '';
-    for (var i = 0; i < books.length; i++) {
-      var book = books[i].volumeInfo;
-      html += '<div class="book-item">';
-      if (book.imageLinks && book.imageLinks.smallThumbnail) {
-        html += '<img src="' + book.imageLinks.smallThumbnail + '" alt="' + book.title + '">';
-      }
-      html += '<div class="book-info">';
-      html += '<h2 class="book-title">' + book.title + '</h2>';
-      if (book.description) {
-        html += '<p class="book-description">' + book.description + '</p>';
-      }
-      html += '</div></div>';
-    }
-    $('#results-container').html(html);
-    displayPagination(page);
-  }
-
-  // Display pagination controls
-  function displayPagination(page) {
-    var html = '';
-    if (totalPages > 1) {
-      html += '<select id="page-select">';
-      for (var i = 1; i <= totalPages; i++) {
-        html += '<option value="' + i + '"';
-        if (i === page) {
-          html += ' selected';
-        }
-        html += '>' + i + '</option>';
-      }
-      html += '</select>';
-    }
-    $('#results-container').append(html);
-
-    // Handle page select change
-    $('#page-select').change(function() {
-      var selectedPage = parseInt($(this).val());
-      displayResults(selectedPage);
+    results.forEach((movie) => {
+        const movieCard = createMovieCard(movie);
+        resultsDiv.appendChild(movieCard);
     });
-  }
+}
+// ... existing code ...
 
+function sortResultsAlphabetically() {
+    const resultsDiv = document.getElementById("results");
+    const movieCards = Array.from(resultsDiv.querySelectorAll(".movie-card"));
 
+    movieCards.sort((a, b) => {
+        const titleA = a.querySelector("h2").textContent.toLowerCase();
+        const titleB = b.querySelector("h2").textContent.toLowerCase();
+        return titleA.localeCompare(titleB);
+    });
 
-//4
-// Handle layout switching
-$('.layout-switch').on('click', function() {
-  $('.layout-switch').removeClass('active');
-  $(this).addClass('active');
-  var layout = $(this).data('layout');
-  if (layout === 'grid') {
-    $('.search-result-item').addClass('grid-view');
-  } else if (layout === 'list') {
-    $('.search-result-item').removeClass('grid-view');
-  }
-});
-
-function displayResults(books) {
-  const template = $('#bookTemplate').html();
-  const isGridView = $('#gridViewCheckbox').prop('checked');
-  const rendered = Mustache.render(template, { books, isGridView });
-  $('#resultContainer').html(rendered);
+    movieCards.forEach((movieCard) => resultsDiv.appendChild(movieCard));
 }
 
-$('#bookshelf-button').click(function() {
-  window.location.href = 'milestone4.html';
-});
-$(document).ready(function() {
-// Global variables to track the current search term and page
-var searchTerm = "";
-var currentPage = 1;
-var resultsPerPage = 10; // Change this value as per your preference
+// ... existing code ...
+// ... (existing code)
 
-// Function to perform a book search based on the user's input
-function searchBooks() {
-  searchTerm = $("#search-input").val();
-  currentPage = 1; // Reset to the first page when performing a new search
-  getSearchResults();
+// Function to sort the search results by release date
+function sortResultsByReleaseDate() {
+    const resultsDiv = document.getElementById("results");
+    const movies = Array.from(resultsDiv.children); // Convert results to an array
+
+    movies.sort((a, b) => {
+        const aReleaseDate = new Date(a.querySelector('p').textContent.split(': ')[1]);
+        const bReleaseDate = new Date(b.querySelector('p').textContent.split(': ')[1]);
+        return bReleaseDate - aReleaseDate;
+    });
+
+    resultsDiv.innerHTML = ""; // Clear the current results
+
+    movies.forEach((movie) => {
+        resultsDiv.appendChild(movie); // Add the sorted movies back to the resultsDiv
+    });
 }
 
-// Function to fetch search results from the Google Books API
-function getSearchResults() {
-  var startIndex = (currentPage - 1) * resultsPerPage;
-  var url = "https://www.googleapis.com/books/v1/volumes?q=" + searchTerm + "&startIndex=" + startIndex + "&maxResults=" + resultsPerPage;
-  
-  // Make an AJAX request to fetch the search results
-  $.ajax({
-    url: url,
-    dataType: "json",
-    success: function(response) {
-      displaySearchResults(response);
-      renderPagination(response.totalItems);
-    },
-    error: function() {
-      $("#search-results").html("An error occurred while fetching the search results.");
+// ... (existing code)
+
+
+function createMovieCard(movie) {
+    function createMovieCard(movie, isFavoritesPage = false) {
+    const movieCard = document.createElement("div");
+    movieCard.className = "movie-card";
+
+    const image = document.createElement("img");
+    image.src = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+    image.alt = movie.title;
+    movieCard.appendChild(image);
+
+    const title = document.createElement("h2");
+    title.textContent = movie.title;
+    movieCard.appendChild(title);
+
+    const releaseDate = document.createElement("p");
+    releaseDate.textContent = `Release Date: ${movie.release_date}`;
+    movieCard.appendChild(releaseDate);
+
+    movieCard.onclick = function () {
+        displayMovieDetails(movie.id);
+    };
+      // Add remove button to the favorites page
+    if (isFavoritesPage) {
+        const removeFromFavoritesButton = document.createElement("button");
+        removeFromFavoritesButton.textContent = "Remove from Favorites";
+        removeFromFavoritesButton.onclick = function (event) {
+            event.stopPropagation(); // Prevent the click from triggering the movie details
+            removeFromFavorites(movie.id);
+            displayFavorites(); // Refresh the displayed favorites list after removal
+        };
+        movieCard.appendChild(removeFromFavoritesButton);
+    } else {
+const addToFavoritesButton = document.createElement("button");
+        addToFavoritesButton.textContent = "Add to Favorites";
+        addToFavoritesButton.onclick = function () {
+            addToFavorites(movie);
+        };
+        movieCard.appendChild(addToFavoritesButton);
     }
+
+    return movieCard;
+}
+ 
+
+function displayMovieDetails(movieId) {
+    const url = `${apiUrl}/movie/${movieId}?api_key=${apiKey}&append_to_response=credits,reviews`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => showDetails(data))
+        .catch((error) => console.log(error));
+}
+
+function showDetails(movie) {
+    const detailsDiv = document.getElementById("details");
+    detailsDiv.innerHTML = "";
+
+    const title = document.createElement("h2");
+    title.textContent = movie.title;
+    detailsDiv.appendChild(title);
+
+    const overview = document.createElement("p");
+    overview.textContent = movie.overview;
+    detailsDiv.appendChild(overview);
+
+    const castTitle = document.createElement("h3");
+    castTitle.textContent = "Cast:";
+    detailsDiv.appendChild(castTitle);
+
+    const castDetails = document.createElement("div");
+    castDetails.id = "cast-details";
+
+    movie.credits.cast.slice(0, 5).forEach((cast) => {
+        const castCard = createCastCard(cast);
+        castDetails.appendChild(castCard);
+    });
+
+    detailsDiv.appendChild(castDetails);
+
+    detailsDiv.style.display = "block";
+}
+
+function createCastCard(cast) {
+    const castCard = document.createElement("div");
+
+    const profileImage = document.createElement("img");
+    profileImage.src = `https://image.tmdb.org/t/p/w185/${cast.profile_path}`;
+    profileImage.alt = cast.name;
+    castCard.appendChild(profileImage);
+
+    const name = document.createElement("h4");
+    name.textContent = cast.name;
+    castCard.appendChild(name);
+
+    const character = document.createElement("p");
+    character.textContent = `Character: ${cast.character}`;
+    castCard.appendChild(character);
+
+    return castCard;
+}
+
+// ... (existing code)
+
+// Add the following code at the end of script.js
+
+function addToFavorites(movie) {
+  const favorites = getFavorites();
+  favorites.push(movie);
+  saveFavorites(favorites);
+}
+
+function removeFromFavorites(movieId) {
+  let favorites = getFavorites();
+  favorites = favorites.filter((movie) => movie.id !== movieId);
+  saveFavorites(favorites);
+}
+
+function getFavorites() {
+  const favoritesJSON = localStorage.getItem("favorites");
+  return favoritesJSON ? JSON.parse(favoritesJSON) : [];
+}
+
+function saveFavorites(favorites) {
+  const favoritesJSON = JSON.stringify(favorites);
+  localStorage.setItem("favorites", favoritesJSON);
+}
+// Function to remove a movie from the favorites list
+function removeFromFavorites(movieId) {
+    let favorites = getFavorites();
+    favorites = favorites.filter((movie) => movie.id !== movieId);
+    saveFavorites(favorites);
+}
+function displayFavorites() {
+  const favorites = getFavorites();
+  const favoritesDiv = document.getElementById("favorites");
+  favoritesDiv.innerHTML = "";
+
+  favorites.forEach((movie) => {
+    const movieCard = createMovieCard(movie);
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove from Favorites";
+    removeButton.onclick = function () {
+      removeFromFavorites(movie.id);
+      displayFavorites();
+    };
+
+    movieCard.appendChild(removeButton);
+    favoritesDiv.appendChild(movieCard);
   });
 }
-
-// Function to display the search results on the page
-function displaySearchResults(data) {
-  var resultsContainer = $("#search-results");
-  resultsContainer.empty();
-  
-  if (data.items && data.items.length > 0) {
-    $.each(data.items, function(index, item) {
-      var title = item.volumeInfo.title;
-      var description = item.volumeInfo.description;
-      var coverImage = item.volumeInfo.imageLinks ? item.volumeInfo.imageLinks.smallThumbnail : "";
-      
-      var resultItem = $("<div>").addClass("result-item");
-      var titleElement = $("<h3>").text(title);
-      var descriptionElement = $("<p>").text(description);
-      var coverImageElement = $("<img>").attr("src", coverImage);
-      
-      resultItem.append(titleElement, coverImageElement, descriptionElement);
-      resultsContainer.append(resultItem);
-    });
-  } else {
-    resultsContainer.html("No results found.");
-  }
 }
-
-// Function to render the pagination controls
-function renderPagination(totalItems) {
-  var totalPages = Math.ceil(totalItems / resultsPerPage);
-  var paginationContainer = $("#pagination");
-  paginationContainer.empty();
-  
-  if (totalPages > 1) {
-    var pageSelect = $("<select>").attr("id", "page-select");
-    
-    for (var i = 1; i <= totalPages; i++) {
-      var option = $("<option>").val(i).text("Page " + i);
-      if (i === currentPage) {
-        option.attr("selected", "selected");
-      }
-      pageSelect.append(option);
-    }
-    
-    pageSelect.change(function() {
-      currentPage = parseInt($(this).val());
-      getSearchResults();
-    });
-    
-    paginationContainer.append(pageSelect);
-  }
-}
-
-// Event listener for the search button click
-$("#search-button").click(function() {
-  searchBooks();
-});
-});
-});
